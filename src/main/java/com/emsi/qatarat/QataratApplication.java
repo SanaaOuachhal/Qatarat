dd6a0186c07e674eeafb9af83cc13782efc3974e
SanaaOuachhal <sanaa.ouachhal.02@gmail.com>
2024-05-04 01:55:17 +0100

    first commit

---
+++ src/main/java/com/emsi/qatarat/QataratApplication.java
@@ -0,0 +1,13 @@
+package com.emsi.qatarat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QataratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QataratApplication.class, args);
+	}
+
+}
